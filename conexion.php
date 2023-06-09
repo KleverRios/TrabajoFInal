9ed20b2f6ad6ed2b790f9008e409fcf014d0ff5a
KleverRios <108413636+KleverRios@users.noreply.github.com>
2023-06-09 14:21:42 -0500

    Create conexion.php

---
+++ conexion.php
@@ -0,0 +1,11 @@
+$servidor = "localhost";
+$usuario = "root";
+$clave = "123456";
+$bd = "proytw";
+$conexion = mysqli_connect($servidor, $usuario, $clave, $bd);
+/*if($conexion){
+    echo 'conexion exitosa';
+}else {
+    echo 'conexion fallida';
+}*/
+?>
