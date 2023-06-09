7531bc72836df1ea39d820da72d8df2209b5289d
KleverRios <108413636+KleverRios@users.noreply.github.com>
2023-06-09 14:36:34 -0500

    Create nosotros.php

---
+++ nosotros.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="ES">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0 ,user-scalable=no,
+    maximum-scale=1, minimum-sacle=1">
+    <title>EAZY-E SPORTS</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="nosotros.css">
+
+</head>
+
+<body class="contenedor">
+    <header class="header">
+        <div class="cab1"></div>
+        <div class="cab2">
+            <div class="logo1">
+                <p>Eazy-E<br>Sports</p>
+            </div>
+            <div>
+                <from id="busqueda">
+                    <input type="text" id="txtBuscar" name="nombre" placeholder="BUSCAR PRODUCTO">
+                    <button type="submit" id="btn" name="Enviar"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </from>
+            </div>
+
+            <div class="login"><a href="index.php">INGRESA/REGISTRARTE</a></div>
+            <div class="megusta">
+                <a href="#"><i class="fa-regular fa-heart"></i></a>
+            </div>
+            <div class="carro">
+                <a href="Marca.php"><i class="fa-solid fa-cart-shopping"></i> S/.00</a>
+            </div>
+
+        </div>
+        <div class="menuPrincipal">
+            <ul class="nava">
+                <li><a href="Index.php">Inicio</a></li>
+                <li><a href="Marca.php">Marca</a></li>
+                <li><a href="Hombres.php">Hombres</a></li>
+                <li><a href="Mujeres.php">Mujeres</a></li>
+                <li><a href="Niños.php">Niños</a></li>
+                <li><a href="Nosotros.php">Nosotros</a></li>
+                <li><a href="Emprende.html">Emprende con eazy esports</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <section>
+        <h2><br>
+            <center>¿QUIENES SOMOS?</center>
+        </h2>
+        <div class="container-card">
+            <div class="card">
+                <img src="imagenes/izquierda.jpg" alt="Logotipo de html5" >
+            </div>
+            <div class="card">
+                <img src="imagenes/quienessomos.jpg.png" alt="Logotipo de html5" >
+            </div>
+            <div class="card">
+                <img src="imagenes/derecha.jpg" alt="Logotipo de html5" >
+            </div>
+        </div>
+    </section>
+
+    <footer class="footer">
+        <div class="piePadre1">
+            <div class="pie1">
+                <div class="log">
+                    <p>Eazy-E<br>Sports</p>
+                </div>
+                <h2>SOMOS TUS ALIADOS PARA EL EXITO</h2>
+                <p> Av. República, La Victoria 15033</p>
+                <p>Telefono: 963214785</p>
+                <p>email: eazy-esports@gmail.com</p>
+            </div>
+            <div class="pie2">
+                <h1>NUESTRAS MARCAS</h1>
+                <a href="ADIDAS.html">ADIDAS</a>
+                <a href="CAT.html">CAT</a>
+                <a href="NIKE.html">NIKE</a>
+                <a href="PUMA.html">PUMA</a>
+                <a href="FILA.html">FILA</a>
+            </div>
+            <div class="pie3">
+                <h1>ENCUENTRANOS EN</h1>
+                <a href="https://www.facebook.com/people/Eazy-E-Sports/100091901793928/?mibextid=ZbWKwL"
+                    target="_blank">
+                    <p>FACEBOOK</p>
+                </a>
+                <a href="#">INSTAGRAM</a>
+                <a href="#">TIKTOK</a>
+                <a href="https://chat.whatsapp.com/G1SlpAXc00kG11lEMoSlqV" target="_blank">
+                    <p>WHATSAPP</p>
+                </a>
+            </div>
+            <div class="pie4">
+                <h1>ENLACES DE INTERES</h1>
+                <a href="index.php">REGISTRARTE</a>
+                <a href="#">TÉRMINOS Y CONDICIONES</a>
+                <a href="#">POLITICAS DE PRIVACIDAD</a>
+                <a href="Emprende.php">CONTACTANOS</a>
+                <a href="#">NOVEDADES</a>
+            </div>
+        </div>
+
+        <div class="piePadre2">
+            <div class="pie5">
+                <p>2023 CREATED BY EAZY-E SPORTS</p>
+            </div>
+            <div class="pie6">
+                <img src="imagenes/pagos1.png" alt="p6" width="255" height="22">
+            </div>
+        </div>
+    </footer>
+
+</body>
+
+</html>
